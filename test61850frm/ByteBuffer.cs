8986547d6a0aefa55db77d94928113e304976d42
IvanZheleznyakov <iz14@yandex.ru>
2020-10-12 16:36:39 +0300

    добавлен класс ByteBuffer

---
+++ test61850frm/ByteBuffer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test61850frm
+{
+    public class ByteBuffer
+    {
+        public byte[] Buffer { get; set; }
+        public int MaxSize { get; set; }
+        public int Size { get; set; }
+    }
+}
